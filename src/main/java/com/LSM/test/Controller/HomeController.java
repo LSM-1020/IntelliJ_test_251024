6b960ca1942b59bf9ca20b34935f922bdeb0c99b
LSM-1020 <smmlee@naver.com>
2025-10-24 12:32:42 +0900

    test파일2

--- src/main/java/com/LSM/test/Controller/HomeController.java
+++ src/main/java/com/LSM/test/Controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.LSM.test.Controller;
 
+import com.LSM.test.dto.Board;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +9,16 @@ public class HomeController {
 
     @GetMapping("/test")
     public String test() {
-        return "hellow Word";
+        return "hello Word";
     }
+
+    @GetMapping("/board")
+    public Board board() {
+        Board board = new Board();
+        board.setName("홍길동");
+        board.setPass("12345");
+        return board;
+    }
+
 }
 
